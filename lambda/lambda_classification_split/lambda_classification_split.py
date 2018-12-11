68a5f3806156dcb392847f277e19a461a0eab866
Chen Wang <cwang138@illinois.edu>
2018-12-11 15:26:51 -0600

    fix classification split method to accept custom column headers

--- lambda/lambda_classification_split/lambda_classification_split.py
+++ lambda/lambda_classification_split/lambda_classification_split.py
@@ -9,9 +9,11 @@ import json
 import re
 import writeToS3 as s3
 
+
 class Classification:
 
-    def __init__(self,awsPath, localSavePath, localReadPath, remoteReadPath):
+    def __init__(self, awsPath, localSavePath, localReadPath, remoteReadPath,
+                 column):
 
         self.localSavePath = localSavePath
         self.awsPath = awsPath
@@ -19,12 +21,12 @@ class Classification:
         # download remote socialmedia data into a temp folder
         # load it into csv
         filename = remoteReadPath.split('/')[-2] + '.csv'
-        self.filename = filename # save it so split function can reuse this name
+        self.filename = filename  # save it so split function can reuse this name
         s3.downloadToDisk(filename, localReadPath, remoteReadPath)
-        
+
         Array = []
         try:
-            with open(localReadPath + filename,'r',encoding='utf-8') as f:
+            with open(localReadPath + filename, 'r', encoding='utf-8') as f:
                 reader = csv.reader(f)
                 for row in reader:
                     try:
@@ -32,7 +34,8 @@ class Classification:
                     except Exception as e:
                         pass
         except:
-            with open(localReadPath + filename,'r',encoding='ISO-8859-1') as f:
+            with open(localReadPath + filename, 'r',
+                      encoding='ISO-8859-1') as f:
                 reader = csv.reader(f)
                 for row in reader:
                     try:
@@ -45,63 +48,48 @@ class Classification:
         # remoteReadPath always follows format of sessionID/folderID/datasetName/
         # example: local/GraphQL/twitter-Tweet/trump/ => ['local','GraphQL', 'twitter-Tweet','trump','']
         source = remoteReadPath.split('/')[2]
-        
-        # find the unique tweet in a corpus
-        if (source == 'twitter-Tweet') and ('text' in Array[0]):
-            self.corpus = list(set(df[df['text']!='']['text'].dropna().astype('str').tolist()))
-        elif (source == 'twitter-Stream') and ('_source.text' in Array[0]):
-            self.corpus = list(set(df[df['_source.text']!='']['_source.text'].dropna().astype('str').tolist()))
 
-        # find the unique content in crimson hexagon
-        elif (source=='crimson-Hexagon') and ('contents' in Array[0]):
-            self.corpus = list(set(df[df['contents'] != '']['contents'].dropna().astype('str').tolist()))
-
-        # find the unique title in reddit posts
-        elif (source=='reddit-Search' or source=='reddit-Post') and 'title' in Array[0]:
-            self.corpus = list(set(df[df['title']!='']['title'].dropna().astype('str').tolist()))
-        elif source =='reddit-Historical-Post' and '_source.title' in Array[0]:
-            self.corpus = list(set(df[df['_source.title']!='']['_source.title'].dropna().astype('str').tolist()))
-
-        # find the unique body in reddit comments
-        elif (source == 'reddit-Comment' or source == 'reddit-Historical-Comment') and 'body' in Array[0]:
-            self.corpus = list(set(df[df['body']!='']['body'].dropna().astype('str').tolist()))
-
-        # TODO: switch reddit comment to elasticsearch endpoint
-        # elif source == 'reddit-Historical-Comment' and '_source.body' in Array[0]:
-        #     self.corpus = list(set(df[df['_source.body']!='']['_source.body'].dropna().astype('str').tolist()))
+        # find the unique text in a corpus
+        self.corpus = list(
+            set(df[df[column] != ''][column].dropna().astype('str').tolist()))
 
         # strip http in the corpus
-        self.corpus = [ re.sub(r"http\S+","",text) for text in self.corpus]
+        self.corpus = [re.sub(r"http\S+", "", text) for text in self.corpus]
 
-    def split(self,ratio):
-        training_set = list(random.sample(self.corpus, int(len(self.corpus)*ratio/100)))
-        testing_set = [item for item in self.corpus if item not in training_set]
+    def split(self, ratio):
+        training_set = list(
+            random.sample(self.corpus, int(len(self.corpus) * ratio / 100)))
+        testing_set = [item for item in self.corpus if
+                       item not in training_set]
 
         # plot a pie chart of the split
-        labels = ['training set data points','unlabeled data points']
+        labels = ['training set data points', 'unlabeled data points']
         values = [len(training_set), len(testing_set)]
-        trace = go.Pie(labels=labels, values = values, textinfo='value')
-        div_split = plot([trace], output_type='div',image='png',auto_open=False, image_filename='plot_img')
+        trace = go.Pie(labels=labels, values=values, textinfo='value')
+        div_split = plot([trace], output_type='div', image='png',
+                         auto_open=False, image_filename='plot_img')
         fname_div_split = 'div_split.html'
-        with open(self.localSavePath + fname_div_split,"w") as f:
+        with open(self.localSavePath + fname_div_split, "w") as f:
             f.write(div_split)
         s3.upload(self.localSavePath, self.awsPath, fname_div_split)
         div_url = s3.generate_downloads(self.awsPath, fname_div_split)
-        
+
         fname1 = 'TRAINING_' + self.filename
         try:
-            with open(self.localSavePath + fname1,'w',newline="",encoding='utf-8') as f:
+            with open(self.localSavePath + fname1, 'w', newline="",
+                      encoding='utf-8') as f:
                 writer = csv.writer(f)
-                writer.writerow(['text','category'])
+                writer.writerow(['text', 'category'])
                 for row in training_set:
                     try:
                         writer.writerow([row])
                     except UnicodeDecodeError:
                         pass
         except:
-            with open(self.localSavePath + fname1,'w',newline="",encoding='ISO-8859-1') as f:
+            with open(self.localSavePath + fname1, 'w', newline="",
+                      encoding='ISO-8859-1') as f:
                 writer = csv.writer(f)
-                writer.writerow(['text','category'])
+                writer.writerow(['text', 'category'])
                 for row in training_set:
                     try:
                         writer.writerow([row])
@@ -110,11 +98,10 @@ class Classification:
         s3.upload(self.localSavePath, self.awsPath, fname1)
         training_url = s3.generate_downloads(self.awsPath, fname1)
 
-
-
         fname2 = 'UNLABELED_' + self.filename
         try:
-            with open(self.localSavePath + fname2,'w',newline="",encoding='utf-8') as f:
+            with open(self.localSavePath + fname2, 'w', newline="",
+                      encoding='utf-8') as f:
                 writer = csv.writer(f)
                 writer.writerow(['text'])
                 for row in testing_set:
@@ -123,7 +110,8 @@ class Classification:
                     except UnicodeDecodeError:
                         pass
         except:
-            with open(self.localSavePath + fname2,'w',newline="",encoding='ISO-8859-1') as f:
+            with open(self.localSavePath + fname2, 'w', newline="",
+                      encoding='ISO-8859-1') as f:
                 writer = csv.writer(f)
                 writer.writerow(['text'])
                 for row in testing_set:
@@ -134,17 +122,18 @@ class Classification:
         s3.upload(self.localSavePath, self.awsPath, fname2)
         unlabeled_url = s3.generate_downloads(self.awsPath, fname2)
 
+        return {'div': div_url, 'training': training_url,
+                'testing': unlabeled_url}
 
-        return {'div': div_url, 'training':training_url, 'testing': unlabeled_url}
 
-
-def lambda_handler(event,context):
-
+def lambda_handler(event, context):
     output = dict()
 
     # arranging the paths
-    awsPath = event['s3FolderName'] + '/ML/classification/' + event['uid'] +'/'
-    localSavePath = '/tmp/' + event['s3FolderName'] + '/ML/classification/' + event['uid'] + '/'
+    awsPath = event['s3FolderName'] + '/ML/classification/' + event[
+        'uid'] + '/'
+    localSavePath = '/tmp/' + event['s3FolderName'] + '/ML/classification/' + \
+                    event['uid'] + '/'
     localReadPath = '/tmp/' + event['s3FolderName'] + '/'
     if not os.path.exists(localSavePath):
         os.makedirs(localSavePath)
@@ -152,13 +141,14 @@ def lambda_handler(event,context):
         os.makedirs(localReadPath)
 
     fname = 'config.json'
-    with open(localSavePath + fname,"w") as f:
-        json.dump(event,f)
+    with open(localSavePath + fname, "w") as f:
+        json.dump(event, f)
     s3.upload(localSavePath, awsPath, fname)
     output['config'] = s3.generate_downloads(awsPath, fname)
-    output['uuid'] = event['uid']    
+    output['uuid'] = event['uid']
 
-    classification = Classification(awsPath, localSavePath, localReadPath, event['remoteReadPath'])
+    classification = Classification(awsPath, localSavePath, localReadPath,
+                                    event['remoteReadPath'], event['column'])
     output.update(classification.split(int(event['ratio'])))
 
     return output
