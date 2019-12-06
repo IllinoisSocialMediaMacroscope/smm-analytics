dd9ae719ea33e0ae10fc2e10639881cf1b677b69
Chen Wang <cwang138@illinois.edu>
2019-12-06 16:46:27 -0600

    minIO is working

--- rabbitmq/bulk_comparison/writeToS3.py
+++ rabbitmq/bulk_comparison/writeToS3.py
@@ -3,7 +3,7 @@ import mimetypes
 import os
 from botocore.client import Config
 
-client = boto3.client('s3', endpoint_url='http://localhost:9000',
+client = boto3.client('s3', endpoint_url='http://minio:9000',
                       aws_access_key_id = os.environ['AWS_ACCESSKEY'],
                       aws_secret_access_key = os.environ['AWS_ACCESSKEYSECRET'],
                       config=Config(signature_version='s3v4'))
--- rabbitmq/command.sh
+++ rabbitmq/command.sh
@@ -4,4 +4,7 @@ EXPORT AWS_ACCESSKEY=${YOUR AWS ACCESSKEY}
 EXPORT AWS_ACCESSKEYSECRET=${YOUR AWS ACESSKEYSECRET}
 ...
 
+# for minIO
+docker run -p 9000:9000 -e "MINIO_ACCESS_KEY=***REMOVED***" -e "MINIO_SECRET_KEY=***REMOVED***" minio/minio server /data
+
 docker-compose up
