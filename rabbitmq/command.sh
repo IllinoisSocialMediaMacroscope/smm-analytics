a99c5792475739dffd961cf3f847abdee1335fb4
Chen Wang <cwang138@illinois.edu>
2019-12-10 10:42:41 -0600

    remove credentials

--- rabbitmq/command.sh
+++ rabbitmq/command.sh
@@ -5,6 +5,6 @@ EXPORT AWS_ACCESSKEYSECRET=${YOUR AWS ACESSKEYSECRET}
 ...
 
 # for minIO
-docker run -p 9000:9000 -e "MINIO_ACCESS_KEY=***REMOVED***" -e "MINIO_SECRET_KEY=***REMOVED***" minio/minio server /data
+docker run -p 9000:9000 -e "MINIO_ACCESS_KEY=" -e "MINIO_SECRET_KEY=" minio/minio server /data
 
 docker-compose up
