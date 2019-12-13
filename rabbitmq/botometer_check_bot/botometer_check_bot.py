201baa355a9eb3e8d2d7d3dc277831d3c61348a2
Chen Wang <cwang138@illinois.edu>
2019-12-13 11:13:56 -0600

    add traceback

--- rabbitmq/botometer_check_bot/botometer_check_bot.py
+++ rabbitmq/botometer_check_bot/botometer_check_bot.py
@@ -1,21 +1,30 @@
 import json
 import botometer
 import pika
+import sys, traceback
 
 
 def botometer_check_bot_handler(ch, method, properties, body):
-    event = json.loads(body)
-    mashape_key = event['mashape_key']
-    twitter_app_auth = {
-        'consumer_key': event['consumer_key'],
-        'consumer_secret': event['consumer_secret'],
-        'access_token': event['access_token'],
-        'access_token_secret': event['access_token_secret'],
-    }
-    bom = botometer.Botometer(wait_on_ratelimit=False,
-                              mashape_key=mashape_key,
-                              **twitter_app_auth)
-    result = bom.check_account(event['screen_name'])
+
+    try:
+        event = json.loads(body)
+        mashape_key = event['mashape_key']
+        twitter_app_auth = {
+            'consumer_key': event['consumer_key'],
+            'consumer_secret': event['consumer_secret'],
+            'access_token': event['access_token'],
+            'access_token_secret': event['access_token_secret'],
+        }
+        bom = botometer.Botometer(wait_on_ratelimit=False,
+                                  mashape_key=mashape_key,
+                                  **twitter_app_auth)
+        result = bom.check_account(event['screen_name'])
+    except BaseException as e:
+        result = {'ERROR':
+                      {'message': str(e),
+                       'traceback': traceback.format_exc()
+                       }
+                  }
 
     # reply to the sender
     ch.basic_publish(exchange="",
