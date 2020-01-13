1e23b14a8f5aca1787e7ecf54a7defb5054aa95c
Chen Wang <cwang138@illinois.edu>
2020-01-13 11:48:21 -0600

    update how to upload files to clowder

--- rabbitmq/clowder_upload_file/rabbitmq_handler.py
+++ rabbitmq/clowder_upload_file/rabbitmq_handler.py
@@ -1,72 +1,44 @@
 import json
 import os
 import traceback
+from urllib.parse import urlparse
 
 import pika
 import requests
 import writeToS3 as s3
-from urllib.parse import urlparse
 
-
-def add_tags(fileID, auth, tags):
-    headers = {'Content-type': 'application/json', 'accept': 'application/json'}
-    payload = json.dumps({'tags': tags})
-    r = requests.post('https://socialmediamacroscope.ncsa.illinois.edu/' +
-                      'clowder/api/files/' + fileID + '/tags',
-                      data=payload,
-                      headers=headers,
-                      auth=auth)
-    if r.status_code != 200:
-        return None
-
-
-def add_descriptions(fileID, auth, descriptions):
-    headers = {'Content-type': 'application/json', 'accept': 'application/json'}
-    payload = json.dumps({'description': descriptions})
-    r = requests.put('https://socialmediamacroscope.ncsa.illinois.edu/clowder' +
-                     '/api/files/' + fileID + '/updateDescription',
-                     data=payload,
-                     headers=headers,
-                     auth=auth)
-    if r.status_code != 200:
-        return None
-
-
-def add_metadata(fileID, auth, metadata):
-    headers = {'Content-type': 'application/json', 'accept': 'application/json'}
-    payload = json.dumps(metadata)
-    r = requests.post('https://socialmediamacroscope.ncsa.illinois.edu' +
-                      '/clowder/api/files/' + fileID + '/metadata',
-                      data=payload,
-                      headers=headers,
-                      auth=auth)
-    if r.status_code != 200:
-        return None
 
 def get_config_json(config_url):
     # download config data to json
-    r_config = requests.get(config_url)
+    # parse url to extract filename, localPath, and awsPath
+    path = urlparse(config_url).path.split('/')
+    filename = path[-1]
+    localPath = os.path.join('/tmp', path[2])
+    if not os.path.exists(localPath):
+        os.makedirs(localPath)
+    awsPath = '/'.join(path[2:-1])
+    try:
+        s3.downloadToDisk(filename, localPath, awsPath)
+    except:
+        raise ValueError('Cannot find file:' + os.path.join(localPath, filename) + ' in the remote storage!')
 
-    if r_config.status_code == 200:
-        return r_config.json()
-    else:
-        return None
+    with open(os.path.join(localPath, filename), 'r') as f:
+        return json.load(f)
 
 
 def rabbitmq_handler(ch, method, properties, body):
     try:
         # basic fields
         event = json.loads(body)
-        headers = {"Content-Type":"multipart/form-data"}
         auth = (event['username'], event['password'])
         dataset_id = event['payload']['dataset_id']
 
-        # config_url = event['payload']['configuration']
-        # config_json = get_config_json(config_url)
+        config_url = event['payload']['configuration']
+        config_json = get_config_json(config_url)
 
-        files = []
+        # upload files
+        file_ids = []
         for file in event['payload']['files']:
-
             # parse url to extract filename, localPath, and awsPath
             path = urlparse(file['url']).path.split('/')
             filename = path[-1]
@@ -76,35 +48,61 @@ def rabbitmq_handler(ch, method, properties, body):
             awsPath = '/'.join(path[2:-1])
             try:
                 s3.downloadToDisk(filename, localPath, awsPath)
-                files.append(('File', open(os.path.join(localPath, filename), 'rb')))
             except:
-                raise ValueError('Cannot find file:' + os.path.join(awsPath, filename) + ' in the remote storage!')
+                raise ValueError('Cannot find file:' + os.path.join(localPath, filename) + ' in the remote storage!')
 
-        r = requests.post(
-            'https://socialmediamacroscope.ncsa.illinois.edu/clowder/api/uploadToDataset/' + dataset_id +
-            '?extract=true',
-            files = files,
-            headers=headers,
-            auth=auth)
+            r = requests.post(
+                'https://socialmediamacroscope.ncsa.illinois.edu/clowder/api/uploadToDataset/' + dataset_id +
+                '?extract=true',
+                files=[('File', open(os.path.join(localPath, filename), 'rb'))],
+                auth=auth)
 
-        if r.status_code != 200:
-            raise ValueError('cannot upload this file: ' + dataset_id)
-        else:
-            file_ids = r.json()['ids']
+            if r.status_code != 200:
+                raise ValueError("cannot upload these files to dataset: " +
+                                 dataset_id + ". error:" + r.text)
+            else:
+                file_ids.append(r.json()['id'])
 
-            # if 'tags' in event['payload'][url].keys():
-            #     if not add_tags(r.json()['id'], auth, event['payload'][url]['tags']):
-            #         raise ValueError('cannot add tags to this file: ' + r.json()['id'])
-            # # add config file to metadata (default)
-            # if not add_metadata(r.json()['id'], auth, config_json):
-            #     raise ValueError('cannot add config metadata to this file: ' + r.json()['id'])
-            # if 'metadata' in event['payload'][url].keys():
-            #     if not add_metadata(r.json()['id'], auth, event['payload'][url]['metadata']):
-            #         raise ValueError('cannot add metadata to this file: ' + r.json()['id'])
-            #
-            # if 'descriptions' in event['payload'][url].keys():
-            #     if not add_descriptions(r.json()['id'], auth, event['payload'][url]['descriptions']):
-            #         raise ValueError('cannot add tags to this file: ' + r.json()['id'])
+            # add config file to metadata (default)
+            config_metadata_r = requests.post('https://socialmediamacroscope.ncsa.illinois.edu' +
+                                              '/clowder/api/files/' + r.json()['id'] + '/metadata',
+                                              data=config_json,
+                                              auth=auth)
+            if config_metadata_r.status_code != 200:
+                raise ValueError('cannot add configuration metadata to this file: ' + r.json()['id'] + ". error: " +
+                                 config_metadata_r.text)
+
+            # add tags
+            if 'tags' in file.keys():
+                tag_payload = json.dumps({'tags': file['tags']})
+                tag_r = requests.post('https://socialmediamacroscope.ncsa.illinois.edu/' +
+                                      'clowder/api/files/' + r.json()['id'] + '/tags',
+                                      data=tag_payload,
+                                      auth=auth)
+                if tag_r.status_code != 200:
+                    raise ValueError('cannot add tags to this file: ' + r.json()['id'] + ". error: " + tag_r.text)
+
+            # add metadata
+            if 'metadata' in file.keys():
+                metadata_payload = json.dumps(file['metadata'])
+                metadata_r = requests.post('https://socialmediamacroscope.ncsa.illinois.edu' +
+                                           '/clowder/api/files/' + r.json()['id'] + '/metadata',
+                                           data=metadata_payload,
+                                           auth=auth)
+                if metadata_r.status_code != 200:
+                    raise ValueError('cannot add metadata to this file: ' + r.json()['id'] + ". error: " +
+                                     metadata_r.text)
+
+            # add description
+            if 'descriptions' in file.keys():
+                description_payload = json.dumps({'description': file['descriptions']})
+                description_r = requests.put('https://socialmediamacroscope.ncsa.illinois.edu/clowder' +
+                                             '/api/files/' + r.json()['id'] + '/updateDescription',
+                                             data=description_payload,
+                                             auth=auth)
+                if description_r.status_code != 200:
+                    raise ValueError(
+                        'cannot add descriptions to this file: ' + r.json()['id'] + ". error: " + description_r.text)
 
         resp = {'info': 'You have successfully uploaded all the files to your specified dataset!',
                 'ids': file_ids}
