619ff884396656f4c41e45a4ec771d700f994549
Chen Wang <cwang138@illinois.edu>
2020-02-10 09:17:31 -0600

    rewrite image_crawler so it works for batch with local minio dataset

--- batch/image_crawler/batch_function.py
+++ batch/image_crawler/batch_function.py
@@ -1,4 +1,4 @@
-import dataset
+from dataset import Dataset
 import argparse
 from notification import notification
 from image_crawler import image_crawler as ic
@@ -13,13 +13,46 @@ if __name__ == '__main__':
     parser.add_argument('--remoteReadPath', required=True)
     parser.add_argument('--email', required=True)
     parser.add_argument('--sessionURL', required=True)
+
+    # user specified parameters
+    parsed, unknown = parser.parse_known_args()
+    for arg in unknown:
+        if arg.startswith("--"):
+            parser.add_argument(arg, required=False)
+
     params = vars(parser.parse_args())
 
+    if 'HOST_IP' in params.keys():
+        HOST_IP = params['HOST_IP']
+        params.pop('HOST_IP', None)
+    else:
+        HOST_IP = None
+
+    if 'AWS_ACCESSKEY' in params.keys():
+        AWS_ACCESSKEY = params['AWS_ACCESSKEY']
+        params.pop('AWS_ACCESSKEY', None)
+    else:
+        AWS_ACCESSKEY = None
+
+    if 'AWS_ACCESSKEYSECRET' in params.keys():
+        AWS_ACCESSKEYSECRET = params['AWS_ACCESSKEYSECRET']
+        params.pop('AWS_ACCESSKEYSECRET', None)
+    else:
+        AWS_ACCESSKEYSECRET = None
+
+    if 'BUCKET_NAME' in params.keys():
+        BUCKET_NAME = params['BUCKET_NAME']
+        params.pop('BUCKET_NAME', None)
+    else:
+        BUCKET_NAME = None
+
+    d = Dataset(HOST_IP, AWS_ACCESSKEY, AWS_ACCESSKEYSECRET, BUCKET_NAME)
+
     # arranging the paths
-    path = dataset.organize_path_lambda(params)
+    path = d.organize_path_lambda(params)
 
     # prepare input dataset
-    df = dataset.get_remote_input(path['remoteReadPath'],
+    df = d.get_remote_input(path['remoteReadPath'],
                                   path['filename'],
                                   path['localReadPath'])
 
@@ -44,8 +77,8 @@ if __name__ == '__main__':
     for img_url in img_urls:
         if ic.is_image(img_url):
             filename, binary = ic.crawler(img_url)
-            dataset.save_local_output(path['localSavePath'], filename, binary)
-    urls['images.zip'] = dataset.save_remote_output(
+            d.save_local_output(path['localSavePath'], filename, binary)
+    urls['images.zip'] = d.save_remote_output(
         path['localSavePath'], path['remoteSavePath'], 'images.zip')
 
     # push notification email
--- batch/image_crawler/dataset.py
+++ batch/image_crawler/dataset.py
@@ -2,105 +2,109 @@ import csv
 import os
 import pandas as pd
 import zipfile
-import writeToS3 as s3
+from writeToS3 import WriteToS3
+
+class Dataset:
+
+    def __init__(self, HOST_IP=None, AWS_ACCESSKEY=None, AWS_ACCESSKEYSECRET=None, BUCKET_NAME=None):
+        self.s3 = WriteToS3(HOST_IP, AWS_ACCESSKEY, AWS_ACCESSKEYSECRET, BUCKET_NAME)
+
+    def organize_path_lambda(self, event):
+        """
+        parse the lambda handler parameter event to construct necessary paths for reading and storing data
+        :param event: aws lambda parameters from handler
+        :return: path dictionary
+        """
+        # arranging the paths
+        remoteReadPath = event['remoteReadPath']
+        localReadPath = os.path.join('/tmp', remoteReadPath)
+        localSavePath = os.path.join('/tmp', remoteReadPath)
+        remoteSavePath = os.path.join(remoteReadPath)
+        filename = remoteReadPath.split('/')[-2] + '.csv'
+
+        if not os.path.exists(localSavePath):
+            os.makedirs(localSavePath)
+            os.makedirs(os.path.join(localSavePath, 'img'))
+        if not os.path.exists(localReadPath):
+            os.makedirs(localReadPath)
+
+        path = {
+            'remoteReadPath': remoteReadPath,
+            'localReadPath': localReadPath,
+            'localSavePath': localSavePath,
+            'remoteSavePath': remoteSavePath,
+            'filename': filename
+        }
+
+        return path
 
 
-def organize_path_lambda(event):
-    """
-    parse the lambda handler parameter event to construct necessary paths for reading and storing data
-    :param event: aws lambda parameters from handler
-    :return: path dictionary
-    """
-    # arranging the paths
-    remoteReadPath = event['remoteReadPath']
-    localReadPath = os.path.join('/tmp', remoteReadPath)
-    localSavePath = os.path.join('/tmp', remoteReadPath)
-    remoteSavePath = os.path.join(remoteReadPath)
-    filename = remoteReadPath.split('/')[-2] + '.csv'
+    def get_remote_input(self, remoteReadPath, filename, localReadPath):
+        """
+        download input file from s3 bucket to a local location, and then load
+        it to a pandas dataframe
+        :param remoteReadPath: remote path in s3 to store the data
+        :param localReadPath: local location to store the data, usually in /tmp
+        :return: df: dataframe that contains the complete input file
+        """
+        self.s3.downloadToDisk(filename, localReadPath, remoteReadPath)
 
-    if not os.path.exists(localSavePath):
-        os.makedirs(localSavePath)
-        os.makedirs(os.path.join(localSavePath, 'img'))
-    if not os.path.exists(localReadPath):
-        os.makedirs(localReadPath)
+        # quick fix for decoding error, sometimes the data is coded in ISO-8859-1
+        # Array = 2D nested list holding column and row data
+        Array = []
+        try:
+            with open(os.path.join(localReadPath, filename), 'r',
+                      encoding='utf-8') as f:
+                reader = csv.reader(f)
+                try:
+                    for row in reader:
+                        Array.append(row)
+                except Exception as e:
+                    print(e)
+        except Exception:
+            with open(os.path.join(localReadPath, filename), 'r',
+                      encoding='ISO-8859-1') as f:
+                reader = csv.reader(f)
+                try:
+                    for row in reader:
+                        Array.append(row)
+                except Exception as e:
+                    print(e)
 
-    path = {
-        'remoteReadPath': remoteReadPath,
-        'localReadPath': localReadPath,
-        'localSavePath': localSavePath,
-        'remoteSavePath': remoteSavePath,
-        'filename': filename
-    }
+        # load to pandas dataframe
+        df = pd.DataFrame(Array[1:], columns=Array[0])
 
-    return path
-
-
-def get_remote_input(remoteReadPath, filename, localReadPath):
-    """
-    download input file from s3 bucket to a local location, and then load
-    it to a pandas dataframe
-    :param remoteReadPath: remote path in s3 to store the data
-    :param localReadPath: local location to store the data, usually in /tmp
-    :return: df: dataframe that contains the complete input file
-    """
-    s3.downloadToDisk(filename, localReadPath, remoteReadPath)
-
-    # quick fix for decoding error, sometimes the data is coded in ISO-8859-1
-    # Array = 2D nested list holding column and row data
-    Array = []
-    try:
-        with open(os.path.join(localReadPath, filename), 'r',
-                  encoding='utf-8') as f:
-            reader = csv.reader(f)
-            try:
-                for row in reader:
-                    Array.append(row)
-            except Exception as e:
-                print(e)
-    except Exception:
-        with open(os.path.join(localReadPath, filename), 'r',
-                  encoding='ISO-8859-1') as f:
-            reader = csv.reader(f)
-            try:
-                for row in reader:
-                    Array.append(row)
-            except Exception as e:
-                print(e)
-
-    # load to pandas dataframe
-    df = pd.DataFrame(Array[1:], columns=Array[0])
-
-    return df
+        return df
 
 
-def save_local_output(localSavePath, fname, output_data):
-    with open(os.path.join(localSavePath, 'img', fname), 'wb') as f:
-        f.write(output_data)
+    def save_local_output(self, localSavePath, fname, output_data):
+        with open(os.path.join(localSavePath, 'img', fname), 'wb') as f:
+            f.write(output_data)
 
 
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            print(file)
-            ziph.write(os.path.join(root, file), file)
+    def zipdir(self, path, ziph):
+        # ziph is zipfile handle
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                print(file)
+                ziph.write(os.path.join(root, file), file)
 
 
-def save_remote_output(localSavePath, remoteSavePath, fname):
-    """
+    def save_remote_output(self, localSavePath, remoteSavePath, fname):
+        """
 
-    :param localSavePath:
-    :param remoteSavePath:
-    :param fname:
-    :param output_data:
-    :return:
-    """
-    zipf = zipfile.ZipFile(os.path.join(localSavePath, fname), 'w',
-                           zipfile.ZIP_DEFLATED)
-    zipdir(os.path.join(localSavePath,'img'), zipf)
-    zipf.close()
+        :param localSavePath:
+        :param remoteSavePath:
+        :param fname:
+        :param output_data:
+        :return:
+        """
+        zipf = zipfile.ZipFile(os.path.join(localSavePath, fname), 'w',
+                               zipfile.ZIP_DEFLATED)
+        self.zipdir(os.path.join(localSavePath,'img'), zipf)
+        zipf.close()
 
-    s3.upload(localSavePath, remoteSavePath, fname)
-    url = s3.generate_downloads(remoteSavePath, fname)
+        self.s3.upload(localSavePath, remoteSavePath, fname)
+        url = self.s3.generate_downloads(remoteSavePath, fname)
 
-    return url
+        return url
