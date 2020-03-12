c6a0b59d8a99f8a5e572936a91ee38576904c6f6
Chen Wang <cwang138@illinois.edu>
2020-03-12 13:41:32 -0500

    add options to compare pamuksuz personality

--- lambda/bae_get_sim_score/get_sim_score.py
+++ lambda/bae_get_sim_score/get_sim_score.py
@@ -23,6 +23,10 @@ def lambda_handler(event, context):
     if 'algorithm' not in event.keys():
         event['algorithm'] =  'IBM-Watson'
 
+    # calculate similarity score
+    vector_a = []
+    vector_b = []
+
     # download and read personality scores
     if event['algorithm'] == 'IBM-Watson':
         try:
@@ -34,8 +38,34 @@ def lambda_handler(event, context):
                 user_data = json.load(f)['personality']
             with open(os.path.join(localPath, event['brand_screen_name'] + '_personality.json'),'r') as f:
                 brand_data = json.load(f)['personality']
+
+            if event['option'] == 'personality_sim_score':
+                for p in user_data['personality']:
+                    vector_a.append(p['percentile'])
+                for p in brand_data['personality']:
+                    vector_b.append(p['percentile'])
+
+            elif event['option'] == 'needs_sim_score':
+                for p in user_data['needs']:
+                    vector_a.append(p['percentile'])
+                for p in brand_data['needs']:
+                    vector_b.append(p['percentile'])
+
+            elif event['option'] == 'values_sim_score':
+                for p in user_data['values']:
+                    vector_a.append(p['percentile'])
+                for p in brand_data['values']:
+                    vector_b.append(p['percentile'])
+            elif event['option'] == 'consumption_sim_score':
+                for p in user_data['consumption_preferences']:
+                    for c in p['consumption_preferences']:
+                        vector_a.append(c['score'])
+                for p in brand_data['consumption_preferences']:
+                    for c in p['consumption_preferences']:
+                        vector_b.append(c['score'])
         except:
             raise ValueError('Cannot find the timeline in the remote storage!')
+
     elif event['algorithm'] == 'TwitPersonality':
         try:
             s3.downloadToDisk(event['user_screen_name'] + '_twitPersonality.json', localPath, awsUserPath)
@@ -46,36 +76,32 @@ def lambda_handler(event, context):
                 user_data = json.load(f)['personality']
             with open(os.path.join(localPath, event['brand_screen_name'] + '_twitPersonality.json'),'r') as f:
                 brand_data = json.load(f)['personality']
+
+            if event['option'] == 'personality_sim_score':
+                for p in user_data['personality']:
+                    vector_a.append(p['percentile'])
+                for p in brand_data['personality']:
+                    vector_b.append(p['percentile'])
+
         except:
             raise ValueError('Cannot find the timeline in the remote storage!')
 
-    # calculate similarity score
-    vector_a = []
-    vector_b = []
-    if event['option'] == 'personality_sim_score':
-        for p in user_data['personality']:
-            vector_a.append(p['percentile'])
-        for p in brand_data['personality']:
-            vector_b.append(p['percentile'])
+    if event['algorithm'] == 'Pamuksuz-Personality':
+        try:
+            s3.downloadToDisk(event['user_screen_name'] + '_utku_personality_average.json', localPath, awsUserPath)
+            s3.downloadToDisk(event['brand_screen_name'] + '_utku_personality_average.json', localPath, awsBrandPath)
 
-    elif event['option'] == 'needs_sim_score':
-        for p in user_data['needs']:
-            vector_a.append(p['percentile'])
-        for p in brand_data['needs']:
-            vector_b.append(p['percentile'])
+            # open json and read in values
+            with open(os.path.join(localPath, event['user_screen_name'] + '_utku_personality_average.json'), 'r') as f:
+                user_data = json.load(f)
+            with open(os.path.join(localPath, event['brand_screen_name'] + '_utku_personality_average.json'), 'r') as f:
+                brand_data = json.load(f)
 
-    elif event['option'] == 'values_sim_score':
-        for p in user_data['values']:
-            vector_a.append(p['percentile'])
-        for p in brand_data['values']:
-            vector_b.append(p['percentile'])
-    elif event['option'] == 'consumption_sim_score':
-        for p in user_data['consumption_preferences']:
-            for c in p['consumption_preferences']:
-                vector_a.append(c['score'])
-        for p in brand_data['consumption_preferences']:
-            for c in p['consumption_preferences']:
-                vector_b.append(c['score'])
+            for metric in user_data.keys():
+                vector_a.append(user_data[metric])
+                vector_b.append(brand_data[metric])
+        except:
+            raise ValueError('Cannot find the timeline in the remote storage!')
 
     try:
         return {'sim_score': cos_sim(vector_a, vector_b)}
