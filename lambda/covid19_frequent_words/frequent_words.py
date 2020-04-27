2f5b47f04f293ca1dbbd61fb537af67c4c514d2f
Chen Wang <cwang138@illinois.edu>
2020-04-27 11:40:13 -0500

    refactoring ashleys frequent words scripts

--- lambda/covid19_frequent_words/frequent_words.py
+++ lambda/covid19_frequent_words/frequent_words.py
@@ -1,15 +1,15 @@
+import csv
 import os
 from urllib.parse import unquote_plus
 
 import nltk
+import pandas as pd
+import plot
+import writeToS3 as s3
 from nltk import ngrams, word_tokenize
 from nltk.probability import FreqDist
 from nltk.stem import WordNetLemmatizer
 
-
-import pandas as pd
-import writeToS3 as s3
-import plot
 
 def lambda_handler(event, context):
     # create local path
@@ -28,23 +28,7 @@ def lambda_handler(event, context):
     df = pd.read_csv(os.path.join(localPath, filename))
 
     # extract hashtag
-    hash = extract_frequent_words(df)
-
-    # plot bar chart (frequency chart)
-    index = hash['hashtags'].values.tolist()[:10]
-    counts = hash['Freq'].values.tolist()[:10]
-    title = 'Top 10 prevalent hashtags (' + filename.split(".")[0] +')'
-    div = plot.plot_bar_chart(index, counts, title)
-
-    # save result and write back to s3
-    hash_filename = filename.split(".")[0]
-
-    hash.to_csv(os.path.join(localPath, hash_filename + "_extracted_hashtag.csv"), index=False)
-    s3.upload("macroscope-paho-covid", localPath, "frequent_words", hash_filename + "_extracted_hashtag.csv")
-
-    with open(os.path.join(localPath, hash_filename + "_extracted_hashtag_frequency.html"), 'w') as f:
-        f.write(div)
-    s3.upload("macroscope-paho-covid", localPath, "frequent_words", hash_filename + "_extracted_hashtag_frequency.html")
+    extract_frequent_words(df, localPath)
 
     return None
 
@@ -73,25 +57,62 @@ def tokenize_no_stop(string):
     return lemma_tokens
 
 
-def extract_frequent_words(df):
+def extract_frequent_words(df, localPath):
     hashtags = ["COVID19", "coronavirus", "COVID_19"]
 
     # filter df by hashtag
     for hashtag in hashtags:
         new_df = df[df['Contents'].str.contains("#" + hashtag, na=False)]
 
-        most_common = FreqDist(tokenize_no_stop(big_string(new_df['Contents'].values))).most_common(10)
-        most_common_bigrams = FreqDist(ngram(tokenize_no_stop(big_string(new_df['Contents'].values)), 2)).most_common(10)
-        most_common_trigrams = FreqDist(ngram(tokenize_no_stop(big_string(new_df['Contents'].values)), 3)).most_common(10)
+        most_common = FreqDist(tokenize_no_stop(big_string(new_df['Contents'].values)))
+        most_common_bigrams = FreqDist(ngram(tokenize_no_stop(big_string(new_df['Contents'].values)), 2))
+        most_common_trigrams = FreqDist(ngram(tokenize_no_stop(big_string(new_df['Contents'].values)), 3))
 
         # Plot and save
+        title = "Most prevalent 10 frequent words and phrases used in #" + hashtag + " tweets"
+        subtitles = ["words", "bigrams", "trigrams"]
+
+        indices = []
+        counts = []
+        for phrases in [most_common.most_common(10),
+                        most_common_bigrams.most_common(10),
+                        most_common_trigrams.most_common(10)]:
+            index = []
+            count = []
+            for item in phrases:
+                if isinstance(item[0], tuple):
+                    index.append(' '.join(item[0]))
+                else:
+                    index.append(item[0])
+                count.append(item[1])
+
+            indices.append(index)
+            counts.append(count)
+
+        div = plot.plot_multiple_bar_chart(indices, counts, title, subtitles)
 
         # upload to s3
+        with open(os.path.join(localPath, hashtag + "_extracted_frequent_words.csv"), "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(['word', 'count'])
+            for row in most_common.most_common():
+                writer.writerow(row)
+        s3.upload("macroscope-paho-covid", localPath, "frequent_phrases", hashtag + "_extracted_frequent_words.csv")
 
-if __name__ == '__main__':
-    df = pd.read_csv('test.csv')
-    extract_frequent_words(df)
+        with open(os.path.join(localPath, hashtag + "_extracted_frequent_bigrams.csv"), "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(['bigram', 'count'])
+            for row in most_common_bigrams.most_common():
+                writer.writerow(row)
+        s3.upload("macroscope-paho-covid", localPath, "frequent_phrases", hashtag + "_extracted_frequent_bigrams.csv")
 
+        with open(os.path.join(localPath, hashtag + "_extracted_frequent_trigrams.csv"), "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(['trigram', 'count'])
+            for row in most_common_trigrams.most_common():
+                writer.writerow(row)
+        s3.upload("macroscope-paho-covid", localPath, "frequent_phrases", hashtag + "_extracted_frequent_trigrams.csv")
 
-
-
+        with open(os.path.join(localPath, hashtag + "_extracted_frequent_phrases.html"), 'w') as f:
+            f.write(div)
+        s3.upload("macroscope-paho-covid", localPath, "frequent_phrases", hashtag + "_extracted_frequent_phrases.html")
--- lambda/covid19_frequent_words/plot.py
+++ lambda/covid19_frequent_words/plot.py
@@ -1,38 +1,25 @@
-from plotly.graph_objs import *
 from plotly.offline import plot
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
-def plot_bar_chart(index, counts, title):
-    """
-    plot bar chart
-    :param index: x - axis, usually the index
-    :param counts: y - axis, usually counts
-    :param title:
-    :return:
-    """
+def plot_multiple_bar_chart(indices, counts, title, subtitles):
+    fig = make_subplots(rows=1, cols=3)
 
-    trace = Bar(x=index, y=counts,
-                marker=dict(color='rgba(200,75,73,1.0)',
-                            line=dict(color='rgba(111,11,9,1.0)',width=2)))
-    layout = Layout(
-        title=title,
+    i = 1
+    for index, count, subtitle in zip(indices, counts, subtitles):
+        fig.append_trace(go.Bar(name=subtitle, x=index, y=count), row=1, col=i)
+        i += 1
+
+    fig.update_layout(
+        title_text=title,
         font=dict(family='Arial', size=12),
-        yaxis=dict(
-            showgrid=True,
-            showline=True,
-            showticklabels=True,
-            linecolor='rgba(102, 102, 102, 0.8)',
-            linewidth=2
-        ),
         margin=dict(
             l=70,
             r=70,
             t=70,
             b=70,
-        )
-    )
-
-    fig = Figure(data=[trace], layout=layout)
+        ))
     div = plot(fig, output_type='div', image='png', auto_open=False, image_filename='plot_img')
 
     return div
