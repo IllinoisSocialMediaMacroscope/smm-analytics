bd770db77655c388a77e04f8ba142f0412784246
Chen Wang <cwang138@illinois.edu>
2020-05-15 11:58:05 -0500

    initialize the crimson sentiment code

---
+++ lambda/covid19_crimson_sentiment/crimson_sentiment.py
@@ -0,0 +1,88 @@
+import json
+import os
+import urllib.request
+from datetime import date, timedelta
+from operator import itemgetter
+
+import plot
+import writeToS3 as s3
+
+
+def getAuthToken():  # provides auth token needed to access Crimson API
+    authToken = os.environ['crimsonAuthToken']
+    authToken = "&auth=" + authToken
+    return authToken
+
+
+def crimson_sentiment(projectStartDate, projectEndDate, localPath):
+    monitorID = os.environ['monitorID']
+    urlStart = "https://api.crimsonhexagon.com/api"
+    fnames = []
+
+    dates = "&start=" + projectStartDate + "&end=" + projectEndDate  # Combines start and end date into format needed for API call
+    authToken = getAuthToken()  # Gets auth token
+    endpoint = "/monitor/results?id="  # endpoint needed for this query
+    urlData = urlStart + endpoint + monitorID + authToken + dates  # Combines all API calls parts into full URL
+
+    webURL = urllib.request.urlopen(urlData)
+
+    if webURL.getcode() == 200:
+
+        data = webURL.read().decode('utf8')
+        theJSON = json.loads(data)
+
+        # write json content
+        fname = "monitorID_" + monitorID + "_extracted_results.json"
+        with open(os.path.join(localPath, fname), 'w') as f:
+            json.dump(theJSON, f, indent=2)
+
+        fnames.append(fname)
+
+        # plot pie charts and save to html
+        results = theJSON["results"][0]
+
+        labels = []
+        values = []
+        for metric in ["categories", "emotions"]:
+            label = []
+            value = []
+            for item in results[metric]:
+                label.append(item['category'])
+                value.append(item['volume'])
+            labels.append(label)
+            values.append(value)
+
+        subtitles = ["Basic Sentiment Categories", "Emotions"]
+        div = plot.plot_multiple_pie_chart(labels, values, subtitles)
+        div_fname = "monitorID_" + monitorID + "_extracted_results.html"
+        with open(os.path.join(localPath, div_fname), 'w') as f:
+            f.write(div)
+
+        fnames.append(div_fname)
+
+    else:
+        raise ValueError("Server Error, No Data" + str(webURL.getcode()))
+
+    return fnames
+
+
+def lambda_handler(event, context):
+    # create local path
+    localPath = os.path.join('/tmp', 'results')
+    if not os.path.exists(localPath):
+        os.makedirs(localPath)
+
+    # collect top sources and plot
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    dayBeforeYesterday = today - timedelta(days=2)
+    fnames = crimson_sentiment(dayBeforeYesterday.strftime("%Y-%m-%d"), yesterday.strftime("%Y-%m-%d"),
+                                 localPath)
+    for fname in fnames:
+        s3.upload("macroscope-paho-covid", localPath, "results", fname)
+
+    return None
+
+
+if __name__ == "__main__":
+    lambda_handler(None, None)
---
+++ lambda/covid19_crimson_sentiment/plot.py
@@ -0,0 +1,36 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from plotly.offline import plot
+
+
+def plot_multiple_pie_chart(labels, values, subtitles):
+    fig = make_subplots(rows=1, cols=2, subplot_titles=subtitles,
+                        specs=[[{"type": "pie"}, {"type": "pie"}]])
+
+    i = 1
+    for label, value, subtitle in zip(labels, values, subtitles):
+        fig.add_trace(go.Pie(name=subtitle, labels=label, values=value,
+                             hoverinfo='label+percent+value',
+                             textinfo='label'), row=1, col=i)
+        i += 1
+
+    fig.update_layout(
+        font=dict(family='Arial', size=12),
+        margin=dict(
+            l=70,
+            r=70,
+            t=70,
+            b=70,
+        ))
+    div = plot(fig, output_type='div', auto_open=False, image_filename='plot_img')
+
+    return div
+
+
+if __name__ == "__main__":
+    labels = [["twitter.com", "tumblr.com", "facebook.com"], ["Blogs", "Comments", "Tumblr", "Forums"]]
+    values = [[0.3, 0.5, 0.2],[0.2, 0.15, 0.25, 0.2]]
+    subtitles = ["Top Sites", "Top Sources"]
+    div = plot_multiple_pie_chart(labels, values, subtitles)
+    with open("tmp.html", "w") as f:
+        f.write(div)
