dc53dd5df49f75b22101b132fef80cb96b02a29d
Chen Wang <cwang138@illinois.edu>
2020-06-22 14:32:52 -0500

    make a dockerized version of interst by region

---
+++ batch/covid19_G_interest_by_region/plot.py
@@ -0,0 +1,20 @@
+import plotly.express as px
+from plotly.offline import plot
+
+
+def plot_geograph(df, key, title):
+    df = df[df[key] != 0]
+    fig = px.scatter_mapbox(df,
+                            lat="Latitude (average)",
+                            lon="Longitude (average)",
+                            # locations="Alpha-3 code",
+                            # locationmode="ISO-3",
+                            color="country",  # which column to use to set the color of markers
+                            hover_name=key,  # column added to hover information
+                            size=key,  # size of markers
+                            zoom=1,
+                            title=title)
+    fig.update_layout(mapbox_style="open-street-map",  height=600)
+    div = plot(fig, output_type='div', auto_open=False, image_filename='plot_img')
+
+    return fig, div
