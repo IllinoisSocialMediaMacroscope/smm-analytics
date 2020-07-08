7cc1df0390f5f60acdc9aab88e30bba8e3838756
Chen Wang <cwang138@illinois.edu>
2020-07-08 16:21:15 -0500

    deploy batch version

--- lambda/covid19_G_interest_by_region/plot.py
+++ lambda/covid19_G_interest_by_region/plot.py
@@ -1,5 +1,15 @@
+import io
+import os
+
+import chart_studio
 import plotly.express as px
+from PIL import Image as PILImage
+from chart_studio.plotly import image as PlotlyImage
 from plotly.offline import plot
+
+# configure chart studio
+chart_studio.tools.set_credentials_file(username=os.environ['CHART_STUDIO_USERNAME'],
+                                        api_key=os.environ['CHART_STUDIO_APIKEY'])
 
 
 def plot_geograph(df, key, title):
@@ -14,7 +24,10 @@ def plot_geograph(df, key, title):
                             size=key,  # size of markers
                             zoom=1,
                             title=title)
-    fig.update_layout(mapbox_style="open-street-map",  height=600)
+    fig.update_layout(mapbox_style="open-street-map", height=600)
     div = plot(fig, output_type='div', auto_open=False, image_filename='plot_img')
 
-    return fig, div
+    img_bytes = PlotlyImage.get(fig)
+    image = PILImage.open(io.BytesIO(img_bytes))
+
+    return div, image
