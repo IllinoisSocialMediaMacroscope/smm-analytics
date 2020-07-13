475e9bf8f1d4ea98a8c5db29367255b8afdc6250
Chen Wang <cwang138@illinois.edu>
2020-07-13 14:35:55 -0500

    remove the default color and auto open

--- batch/covid19_timeline_hashtag/plot.py
+++ batch/covid19_timeline_hashtag/plot.py
@@ -1,4 +1,4 @@
-from plotly.graph_objs import *
+import plotly.graph_objects as go
 from plotly.offline import plot
 import chart_studio
 from PIL import Image as PILImage
@@ -21,19 +21,11 @@ def plot_bar_chart(index, counts, title):
     :return:
     """
 
-    trace = Bar(x=index, y=counts,
-                marker=dict(color='rgba(200,75,73,1.0)',
-                            line=dict(color='rgba(111,11,9,1.0)',width=2)))
-    layout = Layout(
-        title=title,
+    trace = go.Bar(x=index, y=counts)
+    layout = go.Layout(
+        title_text=title,
         font=dict(family='Arial', size=12),
-        yaxis=dict(
-            showgrid=True,
-            showline=True,
-            showticklabels=True,
-            linecolor='rgba(102, 102, 102, 0.8)',
-            linewidth=2
-        ),
+        autosize=True,
         margin=dict(
             l=70,
             r=70,
@@ -42,8 +34,8 @@ def plot_bar_chart(index, counts, title):
         )
     )
 
-    fig = Figure(data=[trace], layout=layout)
-    div = plot(fig, output_type='div', image='png', auto_open=False, image_filename='plot_img')
+    fig = go.Figure(data=[trace], layout=layout)
+    div = plot(fig, output_type='div', auto_open=False, image_filename='plot_img')
 
     img_bytes = PlotlyImage.get(fig)
     image = PILImage.open(io.BytesIO(img_bytes))
