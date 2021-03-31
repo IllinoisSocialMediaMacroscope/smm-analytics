ef8f6f8b2a9f3c07f1f72c487163d003461921f4
Chen Wang <cwang138@illinois.edu>
2021-03-31 09:27:50 -0500

    specify python to 3.7 in dockerfile

--- rabbitmq/smile_docker_push.sh
+++ rabbitmq/smile_docker_push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-docker push socialmediamacroscope/classification_predict:latest
-docker push socialmediamacroscope/classification_split:latest
-docker push socialmediamacroscope/classification_train:latest
-docker push socialmediamacroscope/histogram:latest
+#docker push socialmediamacroscope/classification_predict:latest
+#docker push socialmediamacroscope/classification_split:latest
+#docker push socialmediamacroscope/classification_train:latest
+#docker push socialmediamacroscope/histogram:latest
 docker push socialmediamacroscope/network_analysis:latest
 docker push socialmediamacroscope/preprocessing:latest
 docker push socialmediamacroscope/sentiment_analysis:latest
@@ -11,12 +11,12 @@ docker push socialmediamacroscope/name_entity_recognition:latest
 docker push socialmediamacroscope/autophrase:latest
 docker push socialmediamacroscope/image_crawler:latest
 docker push socialmediamacroscope/collect_reddit_comment:latest
-docker push socialmediamacroscope/clowder_create_collection:latest
-docker push socialmediamacroscope/clowder_create_dataset:latest
-docker push socialmediamacroscope/clowder_create_space:latest
-docker push socialmediamacroscope/clowder_list:latest
-docker push socialmediamacroscope/clowder_upload_file:latest
-docker push socialmediamacroscope/crimson_hexagon_monitors:latest
-docker push socialmediamacroscope/smile_server:latest
-docker push socialmediamacroscope/smile_graphql:latest
-docker push socialmediamacroscope/screen_name_prompt:latest
+#docker push socialmediamacroscope/clowder_create_collection:latest
+#docker push socialmediamacroscope/clowder_create_dataset:latest
+#docker push socialmediamacroscope/clowder_create_space:latest
+#docker push socialmediamacroscope/clowder_list:latest
+#docker push socialmediamacroscope/clowder_upload_file:latest
+#docker push socialmediamacroscope/crimson_hexagon_monitors:latest
+#docker push socialmediamacroscope/smile_server:latest
+#docker push socialmediamacroscope/smile_graphql:latest
+#docker push socialmediamacroscope/screen_name_prompt:latest
