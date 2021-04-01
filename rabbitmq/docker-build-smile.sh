1b53a33854a98624c3945950874b5b43b34ae3f5
Chen Wang <cwang138@illinois.edu>
2021-04-01 12:30:55 -0500

    add a build script

---
+++ rabbitmq/docker-build-smile.sh
@@ -0,0 +1,9 @@
+docker build -t socialmediamacroscope/autophrase:latest autophrase
+docker build -t socialmediamacroscope/collect_reddit_comment:latest collect_reddit_comment
+docker build -t socialmediamacroscope/image_crawler:latest image_crawler
+docker build -t socialmediamacroscope/name_entity_recognition:latest name_entity_recognition
+docker build -t socialmediamacroscope/network_analysis:latest network_analysis
+docker build -t socialmediamacroscope/preprocessing:latest preprocessing
+docker build -t socialmediamacroscope/sentiment_analysis:latest sentiment_analysis
+docker build -t socialmediamacroscope/topic_modeling:latest topic_modeling
+docker build -t socialmediamacroscope/smile_server:latest ../../SMILE/www/
