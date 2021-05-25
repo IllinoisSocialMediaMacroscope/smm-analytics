1a1911b1354c174236b547f069f19ee3dc3f1d62
Chen Wang <cwang138@illinois.edu>
2021-05-25 15:00:37 -0500

    update get_personality

--- rabbitmq/get_personality/get_personality.py
+++ rabbitmq/get_personality/get_personality.py
@@ -4,6 +4,7 @@ import pika
 import requests
 import writeToS3 as s3
 import traceback
+import pandas as pd
 
 
 def get_personality_handler(ch, method, properties, body):
@@ -23,7 +24,12 @@ def get_personality_handler(ch, method, properties, body):
         with open(os.path.join(localPath, screen_name + '_tweets.txt'), 'r') as personality_text:
             headers = {'Content-Type': 'text/plain',
                        'Accept': 'application/json'}
-            body = personality_text.read().encode('utf-8', 'ignore')
+
+            # concatenate the text field to be a paragraph
+            df = pd.read_csv(os.path.join(localPath, screen_name + '_tweets.txt'))
+            tweets = df['text'].tolist()
+            body = '. '.join(tweets).encode('utf-8', 'ignore')
+
             r = requests.post(
                 'https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13&consumption_preferences=true&raw_scores=true',
                 headers=headers, data=body, auth=('apikey', event['apikey']), timeout=300)
