0004952d20e6c3ad11b40cd5794cfd4fa80fb51d
Chen Wang <cwang138@illinois.edu>
2021-05-25 14:51:08 -0500

    fix bug in collect timeline

--- rabbitmq/collect_timeline/collect_timeline.py
+++ rabbitmq/collect_timeline/collect_timeline.py
@@ -20,12 +20,12 @@ def collect_timeline_handler(ch, method, properties, body):
         api = tweepy.API(auth)
 
         tweets = []
-        for status in tweepy.Cursor(api.user_timeline, screen_name=event['screen_name'], count=100).items():
-            tweets.append(status._json['text'].encode('utf-8', 'ignore').decode())
+        for status in tweepy.Cursor(api.user_timeline, screen_name=event['screen_name'], count=100,
+                                    tweet_mode="extended").items():
+            tweets.append([status._json['id'], status._json['full_text'].encode('utf-8', 'ignore').decode()])
 
         if len(tweets) > 0:
             fname = event['screen_name'] + '_tweets.txt'
-
             with open(os.path.join(localSavePath, fname), 'w', encoding='utf-8', newline='') as f:
                 header = ['id', 'text']
                 writer = csv.writer(f, delimiter=",")
