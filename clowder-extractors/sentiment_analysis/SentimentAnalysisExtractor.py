e34e0530af4a63c80c4a8a6dfc999c8a3e1aa60f
Chen Wang <cwang138@illinois.edu>
2023-02-03 15:57:01 -0600

    get the skeleton of a clowder extractor set up

---
+++ clowder-extractors/sentiment_analysis/SentimentAnalysisExtractor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""Example extractor based on the clowder code."""
+
+import logging
+from pyclowder.extractors import Extractor
+import pyclowder.files
+
+from algorithm import algorithm
+
+
+class SentimentAnalysisExtractor(Extractor):
+    """Count the number of characters, words and lines in a text file."""
+    def __init__(self):
+        Extractor.__init__(self)
+
+        # parse command line and load default logging configuration
+        self.setup()
+
+        # setup logging for the exctractor
+        logging.getLogger('pyclowder').setLevel(logging.DEBUG)
+        logging.getLogger('__main__').setLevel(logging.DEBUG)
+
+    def process_message(self, connector, host, secret_key, resource, parameters):
+        # this extractor runs on dataset
+        # uncomment to see the resource
+        logger = logging.getLogger(__name__)
+        inputfile = resource["local_paths"][0]
+        dataset_id = resource['parent'].get('id')
+
+        df = pd.read_csv(inputfile)
+        connector.message_process(resource, "Loading contents of file...")
+
+        # execute the algorithm
+        # TODO figure out how to pass in parameters
+        params = {'column':'text',
+                  'algorithm': 'vader'}
+        output = algorithm(df, params)
+        connector.message_process(resource, "Running the algorithm...")
+
+        # upload object to s3 bucket and return the url
+        for fname, output_data in output.items():
+            if fname != 'uid':
+                local_output_path = dataset.save_local_output("", fname, output_data)
+                pyclowder.files.upload_to_dataset(connector, host, secret_key, dataset_id, local_output_path)
+                connector.message_process(resource, "Saving " + local_output_path + "...")
+
+
+if __name__ == "__main__":
+    extractor = SentimentAnalysisExtractor()
+    extractor.start()
--- clowder-extractors/sentiment_analysis/dataset.py
+++ clowder-extractors/sentiment_analysis/dataset.py
@@ -4,93 +4,17 @@ import json
 import pickle
 import pandas as pd
 import types
-import writeToS3 as s3
 
 
-def organize_path_lambda(event):
+def save_local_output(localSavePath, fname, output_data):
     """
-    parse the lambda handler parameter event to construct necessary paths for reading and storing data
-    :param event: aws lambda parameters from handler
-    :return: path dictionary
-    """
-    # arranging the paths
-    localReadPath = os.path.join('/tmp', event['s3FolderName'], event['uid'])
-    localSavePath = os.path.join('/tmp',
-                                 event['s3FolderName'] + event['resultPath'],
-                                 event['uid'])
-    remoteSavePath = os.path.join(event['s3FolderName'] + event['resultPath'],
-                                  event['uid'])
-    if 'remoteReadPath' not in event.keys() or event['remoteReadPath'] == 'undefined':
-        remoteReadPath = remoteSavePath
-        filename = event['labeledFilename']
-    else:
-        remoteReadPath = event['remoteReadPath']
-        filename = remoteReadPath.split('/')[-2] + '.csv'
-
-    if not os.path.exists(localSavePath):
-        os.makedirs(localSavePath)
-    if not os.path.exists(localReadPath):
-        os.makedirs(localReadPath)
-
-    path = {
-        'remoteReadPath': remoteReadPath,
-        'localReadPath': localReadPath,
-        'localSavePath': localSavePath,
-        'remoteSavePath': remoteSavePath,
-        'filename': filename
-    }
-
-    return path
-
-
-def get_remote_input(remoteReadPath, filename, localReadPath):
-    """
-    download input file from s3 bucket to a local location, and then load
-    it to a pandas dataframe
-    :param remoteReadPath: remote path in s3 to store the data
-    :param localReadPath: local location to store the data, usually in /tmp
-    :return: df: dataframe that contains the complete input file
-    """
-    s3.downloadToDisk(filename, localReadPath, remoteReadPath)
-
-    # quick fix for decoding error, sometimes the data is coded in ISO-8859-1
-    # Array = 2D nested list holding column and row data
-    Array = []
-    try:
-        with open(os.path.join(localReadPath, filename), 'r',
-                  encoding='utf-8', errors="ignore") as f:
-            reader = csv.reader(f)
-            try:
-                for row in reader:
-                    Array.append(row)
-            except Exception as e:
-                print(e)
-    except Exception:
-        with open(os.path.join(localReadPath, filename), 'r',
-                  encoding='ISO-8859-1', errors="ignore") as f:
-            reader = csv.reader(f)
-            try:
-                for row in reader:
-                    Array.append(row)
-            except Exception as e:
-                print(e)
-
-    # load to pandas dataframe
-    df = pd.DataFrame(Array[1:], columns=Array[0])
-
-    return df
-
-
-def save_remote_output(localSavePath, remoteSavePath, fname, output_data):
-    """
-    save output in memory first to local file, then upload to remote S3 bucket
+    save output in memory first to local file
     :param localSavePath: local saved file
     :param remoteSavePath: remote save file path
     :param fname: filename
     :param output_data: the actual data
-    :return: url of the file saved in S3 bucket
+    :return: local saved file path
     """
-
     # json
     if isinstance(output_data, dict):
         fname += '.json'
@@ -142,7 +66,4 @@ def save_remote_output(localSavePath, remoteSavePath, fname, output_data):
         with open(os.path.join(localSavePath, fname), 'wb') as f:
             pickle.dump(output_data, f)
 
-    s3.upload(localSavePath, remoteSavePath, fname)
-    url = s3.generate_downloads(remoteSavePath, fname)
-
-    return url
+    return os.path.join(localpath, filename)
