6cf4233837c4e5b315917985b0cac407eb3b6a59
Chen Wang <cwang138@illinois.edu>
2023-02-20 13:54:40 -0600

    add metaddata; fix wordnet

--- clowder-extractors/sentiment_analysis/SentimentAnalysisExtractor.py
+++ clowder-extractors/sentiment_analysis/SentimentAnalysisExtractor.py
@@ -34,9 +34,12 @@ class SentimentAnalysisExtractor(Extractor):
         connector.message_process(resource, "Loading contents of file...")
 
         # execute the algorithm
-        # TODO figure out how to pass in parameters
-        params = {'column':'text',
-                  'algorithm': 'vader'}
+        # Parse user parameters to determine which column to analyze
+        userParams = parameters.get('parameters')
+        column = userParams.get('column', 'text')
+        selected_algorithm = userParams.get('algorithm', 'vader')
+        params = {'column': column,
+                  'algorithm': selected_algorithm}
         output = algorithm(df, params)
         connector.message_process(resource, "Running the algorithm...")
 
@@ -44,8 +47,14 @@ class SentimentAnalysisExtractor(Extractor):
         for fname, output_data in output.items():
             if fname != 'uid':
                 local_output_path = dataset.save_local_output("", fname, output_data)
-                pyclowder.files.upload_to_dataset(connector, host, secret_key, dataset_id, local_output_path)
+                uploaded_file_id = pyclowder.files.upload_to_dataset(connector, host, secret_key, dataset_id,
+                                                                 local_output_path)
                 connector.message_process(resource, "Saving " + local_output_path + "...")
+
+                # write params to metadata
+                metadata = self.get_metadata(params, 'file', uploaded_file_id, host)
+                pyclowder.files.upload_metadata(connector, host, secret_key, uploaded_file_id, metadata)
+                connector.message_process(resource, "Writing metadata...")
 
 
 if __name__ == "__main__":
